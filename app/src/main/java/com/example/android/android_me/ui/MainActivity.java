3f7ac1a4b27ad7eaa96899b732f0a0480b6f7520
Farrukhbek <farruhzokirov00@gmail.com>
2020-01-31 17:31:58 +0500

    made interface in masterListFragment to communicate to main activity

--- app/src/main/java/com/example/android/android_me/ui/MainActivity.java
+++ app/src/main/java/com/example/android/android_me/ui/MainActivity.java
@@ -2,15 +2,20 @@ package com.example.android.android_me.ui;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 import com.example.android.android_me.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MasterListFragment.OnImageClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
+    public void onImageSelected(int position) {
+
+        Toast.makeText(this, "Position clicked " + position, Toast.LENGTH_LONG).show();
     }
 }
--- app/src/main/java/com/example/android/android_me/ui/MasterListFragment.java
+++ app/src/main/java/com/example/android/android_me/ui/MasterListFragment.java
@@ -1,16 +1,35 @@
 package com.example.android.android_me.ui;
 
 import android.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.GridView;
 
 import com.example.android.android_me.R;
 import com.example.android.android_me.data.AndroidImageAssets;
 
 public class MasterListFragment extends Fragment {
+
+    OnImageClickListener mCallback;
+
+    public interface OnImageClickListener {
+        void onImageSelected(int position);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        try {
+            mCallback = (OnImageClickListener) context;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(context.toString() + "must implement OnImageClickListener");
+        }
+    }
 
     public MasterListFragment() {
 
@@ -31,6 +50,13 @@ public class MasterListFragment extends Fragment {
 
         // Set the adapter on the GridView
         gridView.setAdapter(mAdapter);
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                mCallback.onImageSelected(position);
+            }
+        });
 
         // Return the root view
         return rootView;
